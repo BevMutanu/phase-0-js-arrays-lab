899405e3ac1773d1ec690dfdac2c8259867b4475
BevMutanu <bev4366@gmail.com>
2026-03-24 23:02:56 +0300

    commit all changes

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,9 +1,40 @@
 // Write your code here
+let products = ["laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct(){
+  console.log(products[0]);
+}
+
+
+function addProduct(newProduct){
+  products.push(newProduct);
+  return products;
+}
+
+addProduct("Mouse");
+
+
+function updateProductName(position, newName){
+  if (position >= 0 && position < products.length){
+    products[position] = newName;
+    console.log("Product list updated successfully!")
+  }
+  else{
+    console.log("Error: That position does not exist.")
+  }
+  return products;
+}
+
+
+function removeLastProduct(){
+  products.pop();
+  console.log(products);
+}
 
 
 
 // Export the necessary parts for testing
-module.exports = {
+module.exports = { 
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
